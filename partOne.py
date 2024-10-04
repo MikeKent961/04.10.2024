c18fa8127cf34379fe68570798eb45c0820c3baa
MikeKent961 <mk961@kent.ac.uk>
2024-10-04 11:12:07 +0100

    Create partOne.py

---
+++ partOne.py
@@ -0,0 +1,10 @@
+def main():
+    slow = input("Input ")
+    myFunction(slow)
+    
+
+def myFunction(text):
+  print(text.replace(" ", "...")) #prints out the inputted text after replacing the " " white space with "..."
+  
+
+main()
